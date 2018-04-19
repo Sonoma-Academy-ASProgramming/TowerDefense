f9ef1e380e96ea1ff0cf449f88076521031384ad
magilla121 <liev.haroche@sonomaacademy.org>
2018-04-19 17:22:06 -0400

    bugs fixed

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function preload() {
   cannonImg = loadImage('./icons/cannonIcon.png');
 }
 function setup() {
+    frameRate(60);
     UI = null;
     //Center all balls
     cannonImg.resize(80, 80);
@@ -26,14 +27,7 @@ function setup() {
     }
     Towers.push(new Plot(250, 250));
     generateEnemies(100);
-    Shoots.push(new Shoot(250, 250, 1));
 }
-
-setInterval(function() {
-    Shoots.forEach((item) => {
-        item.fire();
-    });
-}, 150);
 
 //GAME LOGIC
 function draw() {
@@ -42,9 +36,7 @@ function draw() {
     Enemies.forEach((enemy) => {
         enemy.draw();
     });
-    Shoots.forEach((shoot) => {
-        shoot.draw();
-    });
+
     Towers.forEach((tower) => {
         tower.update();
     });
@@ -157,7 +149,6 @@ Enemy.prototype.draw = function() {
     this.time -= this.speed;
     this.xPos = getPosition(this.time).x;
     this.yPos = getPosition(this.time).y;
-    this.radius = ((50/this.futureHealth) * 40);
     fill(255/this.futureHealth * 10, 0, 0);
     ellipse(this.xPos, this.yPos, this.radius, this.radius);
     if (this.time <= 0) {
@@ -170,77 +161,6 @@ var mouseInArea = (xMin, xMax, yMin, yMax) => {
         return true;
     }
 }
-class Shoot {
-    constructor(towerX, towerY, force, howManyframesForTheBulletToGetToTheEnemy) {
-        this.Bullets = [];
-        this.x = towerX;
-        this.y = towerY;
-        this.force = force || 1;
-        this.time = howManyframesForTheBulletToGetToTheEnemy || 50;
-    }
-}
-
-Shoot.prototype.findEnemy = function(howManyth) {
-    let sortedEnemies = Enemies.slice(0).sort((a, b) => {
-        return b.time - a.time;
-    });
-    return sortedEnemies[howManyth];
-}
-
-Shoot.prototype.fire = function() {
-    let enemy = null;
-    let tmp;
-    for (let i = 0; i < Enemies.length; i++) {
-      try{
-        tmp = this.findEnemy(i);
-        if (tmp.futureHealth > 0 && tmp.time - this.time>0) {
-            enemy = tmp;
-            break;
-        }
-      }catch (e){}
-    }
-    if (!enemy) { //no valid enemies
-        return;
-    }
-    enemy.futureHealth -= this.force;
-    let aimFor = getPosition(enemy.time - this.time);
-    let Xinc = (aimFor.x - this.x) / this.time,
-        Yinc = (aimFor.y - this.y) / this.time;
-    let newObj = {
-        x: this.x,
-        y: this.y,
-        aimX: aimFor.x,
-        aimY: aimFor.y,
-        Xinc,
-        Yinc,
-        force: this.force,
-        type: this.type,
-        enemy
-    };
-
-    this.Bullets.push(newObj);
-}
-
-Shoot.prototype.update = function() {
-    this.Bullets.forEach((item, index) => {
-        item.x += item.Xinc;
-        item.y += item.Yinc;
-        //CHECK FOR COLLISION
-        if (Math.sqrt((item.enemy.xPos - item.x) * (item.enemy.xPos - item.x) + (item.enemy.yPos - item.y) * (item.enemy.yPos - item.y)) < item.enemy.radius / 2) {
-            item.enemy.hit(this.force);
-            this.Bullets.splice(index, 1);
-        }
-    });
-}
-
-Shoot.prototype.draw = function() {
-    this.update();
-    this.Bullets.forEach((item) => {
-        fill('black');
-        ellipse(item.x, item.y, 5, 5);
-    });
-}
-
 
 function getX(time){
   return sin(time);
