30b28e51971cb06dfff10e7cb2e2cc93c9a13d1b
Sean-2002 <33432158+Sean-2002@users.noreply.github.com>
2018-05-11 13:46:52 -0700

    Features added, bugs fixed.

--- scripts/score.js
+++ scripts/score.js
@@ -9,25 +9,27 @@ let score = {
     coinIMG: NaN,
     coinRotate: 0,
     coinLeft: 0,
-    coinTop:0,
+    coinTop: 0,
+    lastLevel: 1,
+    levelUpPos: 0,
     getSize(x) {
         if (x > 20) {
             return 28;
         }
         return -0.08 * x * x + 1.6 * x + 28;
     },
-    getLevelUpYPos() {
-        if (x > 0) {
+    getLevelUpYPos(x) { //(0 <= x <= 100)
+        if (x <0) {
             return -10000;
         }
-        return (-1 + 0.0595833 * x - 0.0011875 * x * x + 7.91667 * Math.pow(10, -6) * x * x * x) * height;
+        return ((-1 + 0.0595833 * x - 0.0011875 * x * x + 7.91667 * Math.pow(10, -6) * x * x * x)+.5) * height;
 
     },
     drawScore() {
         textFont(gameFont);
         this.drawscore();
-        this.drawLevel();
         this.drawMoney();
+        this.drawLevel();
     },
     drawscore() {
         if (Game.score - this.flastInterval >= this.lastFlash) {
@@ -43,15 +45,29 @@ let score = {
         fill(0, 0, 0);
         textSize(30);
         text("Level: " + Game.level, this.levelLeft, this.coinTop);
+        push();
+        imageMode(CENTER);
+        translate(width *0.5-150, this.getLevelUpYPos(this.levelUpPos));
+        textSize(100);
+        rotate(radians(30));
+        text("Level " + Game.level, 0, 0)
+        pop();
+        this.levelUpPos -= 1;
+        if (this.lastLevel !== Game.level) {
+            this.levelUpPos = 100;
+            this.lastLevel = Game.level;
+        }
+        // console.log(this.getLevelUpYPos(this.levelUpPos))
     },
     drawMoney() {
         push();
         imageMode(CENTER);
-        translate(this.coinLeft,this.scoreHeight-height*0.025);
+        translate(this.coinLeft, this.scoreHeight - height * 0.025);
         rotate(Math.sin((this.coinRotate < 9.42) ? this.coinRotate : 0));
-        image(this.coinIMG, 0, 0, 30,30);
+        image(this.coinIMG, 0, 0, 30, 30);
         pop();
-        text(Game.money, this.coinLeft+70,this.scoreHeight);
+        // textSize(30);
+        text(Game.money, this.coinLeft + 45, this.coinTop);
         if (this.coinRotate > 10 * 3.14) {
             this.coinRotate = 0;
         }
