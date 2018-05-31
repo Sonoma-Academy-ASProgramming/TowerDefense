ef23d826323b261513ff3d62f107deace5ae215b
magilla121 <liev.haroche@sonomaacademy.org>
2018-05-31 15:04:32 -0700

    Added new empty plots and a working credits screen

--- scripts/game.js
+++ scripts/game.js
@@ -4,6 +4,7 @@ let startButton;
 let tutorialButton;
 let leaderBoardButton;
 let creditsButton;
+let returnToMainMenuButton;
 
 let GameStates = {
     'GameStart': 0,
@@ -20,6 +21,16 @@ let Game = {
     gameTime: 0,
     gameState: GameStates.GameStart,
     spawning: 10,
+    startMenu: () => {
+      console.log('starting');
+      Sprites = [];
+      spriteCount = 0;
+      console.log('partially cleared');
+      console.log('cleared');
+      Game.gameState = GameStates.GameStart;
+      console.log(Game.gameState);
+      setupStartScreen();
+    },
     startGame: () => {
         /*Define Variables*/
         Game.spawning = 10;
@@ -28,6 +39,12 @@ let Game = {
         Game.money = 110;
         Game.gameTime = 0;
         Game.gameState = GameStates.InGame;
+        backgroundSprite = new Supersprite(width / 2, height / 2, width, height);
+        backgroundSprite.addImage(backgroundImg);
+        backgroundSprite.onMousePressed = () => {
+            UI.delete();
+            selectedTower = null;
+        };
         try {
             replayButton.delete();
             exitButton.delete();
@@ -65,36 +82,57 @@ let Game = {
         replayButton = new Supersprite(horizontal(35), vertical(90), 200, 75, {type: 'button', text: "Replay"});
         exitButton = new Supersprite(horizontal(70), vertical(90), 200, 75, {type: 'button', text: "Leaderboard"});
         replayButton.onMousePressed = () => {
+            buttonSound.play();
             Game.startGame()
         };
         exitButton.onMousePressed = () => {
+          buttonSound.play();
             Game.gameState = GameStates.LeaderBoard;
         };
         Game.gameState = GameStates.GameOver;
     },
-
+    showCredits: () => {
+      Sprites = [];
+      spriteCount = 0;
+      returnToMainMenuButton = new Supersprite(horizontal(50), vertical(86), 200, 75, {type: 'button', text: "Back"});
+      returnToMainMenuButton.onMousePressed = () => {
+        buttonSound.play();
+        Game.startMenu();
+      }
+      Game.gameState = GameStates.Credits;
+    }
 };
 
 function setupStartScreen() {
-  startButton = new Supersprite(horizontal(50), vertical(30), 200, 75, {type: 'button', text: "Start Game"});
+  startButton = new Supersprite(horizontal(50), vertical(50), 200, 75, {type: 'button', text: "Start Game"});
   startButton.onMousePressed = () => {
+    buttonSound.play();
     Game.startGame();
   };
-  tutorialButton = new Supersprite(horizontal(50), vertical(50), 200, 75, {type: 'button', text: "Tutorial"});
+  tutorialButton = new Supersprite(horizontal(50), vertical(62), 200, 75, {type: 'button', text: "Tutorial"});
   tutorialButton.onMousePressed = () => {
-    Window.open('https://www.roblox.com', _blank);
+    buttonSound.play();
+    window.open('https://www.roblox.com', '_blank');
   };
-  leaderBoardButton = new Supersprite(horizontal(50), vertical(70), 200, 75, {type: 'button', text: "View Leaderboard"});
+  leaderBoardButton = new Supersprite(horizontal(50), vertical(74), 200, 75, {type: 'button', text: "View Leaderboard"});
   leaderBoardButton.onMousePressed = () => {
+    buttonSound.play();
     console.log('opening leaderboard');
   };
-  creditsButton = new Supersprite(horizontal(50), vertical(85), {type: 'button', text: "Credits"});
+  creditsButton = new Supersprite(horizontal(50), vertical(86), 200, 75, {type: 'button', text: "Credits"});
   creditsButton.onMousePressed = () => {
-    console.log('This is a game made by some people');
+    buttonSound.play();
+    Game.showCredits();
   }
 }
 
 function StartScreen() {
+  push();
+  fill('#736357');
+  textAlign(CENTER, BOTTOM);
+  textSize(60);
+  text("GAC Attack!", horizontal(50), vertical(30));
+  pop();
   startButton.display();
   tutorialButton.display();
   leaderBoardButton.display();
@@ -110,4 +148,20 @@ function EndScreen() {
     textSize(60);
     text("Game Over!", horizontal(50), vertical(30));
     pop();
+}
+
+function CreditScreen() {
+  push();
+  fill('#736357');
+  textAlign(CENTER, BOTTOM);
+  textSize(60);
+  text("GAC Attack!", horizontal(50), vertical(30));
+  textSize(35);
+  text("Game made by:", horizontal(50), vertical(40));
+  textSize(25);
+  text("Kheva Mann", horizontal(50), vertical(50));
+  text("Liev Haroche", horizontal(50), vertical(60));
+  text("Sean Sun", horizontal(50), vertical(70));
+  pop();
+  returnToMainMenuButton.display();
 }
